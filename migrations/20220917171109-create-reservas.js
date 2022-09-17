f0d02bd51f4447515adb25e19da599faf20d736a
erika99 <erimaca@misena.edu.co>
2022-09-17 13:37:02 -0500

    trabajo

---
+++ migrations/20220917171109-create-reservas.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('reservas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      idreservas: {
+        type: Sequelize.INTEGER
+      },
+      inicio_fecha: {
+        type: Sequelize.DATE
+      },
+      fin_fecha: {
+        type: Sequelize.DATE
+      },
+      idhabitaciones: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'habitaciones',
+          },
+          key: 'id'
+        },
+        allowNull: false
+      },
+      idhespedes: {
+        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: 'huespedes',
+          },
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('reservas');
+  }
+};
